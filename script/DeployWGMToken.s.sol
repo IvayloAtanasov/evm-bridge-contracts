2ce131ed49010d5610e88d9dc40e0ee03573b171
Ivaylo Atanasov <aivo@devlabs.bg>
2025-06-15 20:05:48 +0300

    adds bridge and wrapped token

--- script/DeployWGMToken.s.sol
+++ script/DeployWGMToken.s.sol
@@ -2,17 +2,22 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "../src/BridgeFactory.sol";
+import "../src/WERC20.sol";
 
-contract DeployBridgeFactory is Script {
+contract DeployWGMToken is Script {
   function setUp() public {}
 
   function run() public {
     uint256 deployer = vm.envUint("PRIVATE_KEY");
     vm.startBroadcast(deployer);
 
-    address relayer = 0xe64c80DaC84aeE6983C3a2945a84f757e98c6B40;
-    new BridgeFactory(relayer);
+    address bridge = 0xe64c80DaC84aeE6983C3a2945a84f757e98c6B40; // TODO: bridge
+    new WERC20(
+      "Wrapped Goodmorning",
+      "WGM",
+      6,
+      bridge
+    );
 
     vm.stopBroadcast();
   }
--- src/BridgeFactory.sol
+++ src/BridgeFactory.sol
@@ -12,22 +12,49 @@ interface IERC20Permit {
     bytes32 s
   ) external;
 
+  function transfer(address to, uint256 amount) external returns (bool);
+
   function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
 
+interface IWERC20Permit {
+  function mint(address to, uint256 amount) external;
+
+  function burn(address from, uint256 amount) external;
+}
+
 contract BridgeFactory {
-  event Bridged(
+  event Locked(
+    address indexed sender,
     address indexed user,
     address indexed token,
     uint256 amount,
-    address indexed to,
     uint256 targetChainId
   );
 
+  event Unlocked(
+    address indexed sender,
+    address indexed user,
+    address indexed token,
+    uint256 amount,
+    uint256 targetChainId
+  );
+
+  address public relayer;
+
+  constructor(address _relayer) {
+    relayer = _relayer;
+  }
+
+  modifier onlyRelayer() {
+    require(msg.sender == relayer, "Not authorized relayer");
+    _;
+  }
+
   function lockWithPermit(
+    address user,
     address token,
     uint256 amount,
-    address to,
     uint256 targetChainId,
     uint256 deadline,
     uint8 v,
@@ -35,7 +62,7 @@ contract BridgeFactory {
     bytes32 s
   ) external {
     IERC20Permit(token).permit(
-      msg.sender,
+      user,
       address(this),
       amount,
       deadline,
@@ -44,9 +71,28 @@ contract BridgeFactory {
       s
     );
 
-    bool success = IERC20Permit(token).transferFrom(msg.sender, address(this), amount);
+    bool success = IERC20Permit(token).transferFrom(user, address(this), amount);
     require(success, "Transfer failed");
 
-    emit Bridged(msg.sender, token, amount, to, targetChainId);
+    emit Locked(msg.sender, user, token, amount, targetChainId);
+  }
+
+  function claimWrapped(address user, address wrappedToken, uint256 amount) external onlyRelayer {
+    IWERC20Permit(wrappedToken).mint(user, amount);
+  }
+
+  function unwrap(
+    address wrappedToken,
+    uint256 amount,
+    uint256 targetChainId
+  ) external {
+    address user = msg.sender;
+    IWERC20Permit(wrappedToken).burn(user, amount);
+
+    emit Unlocked(msg.sender, user, wrappedToken, amount, targetChainId);
+  }
+
+  function claim(address user, address token, uint256 amount) external onlyRelayer {
+    IERC20Permit(token).transfer(user, amount);
   }
 }
